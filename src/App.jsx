08882e3ef01bf62c3ffaa1cdfa736e11d0efa3c0
Ajith <ajithpkumar231@gmail.com>
2024-10-05 07:34:46 +0530

    added all movies page

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,38 @@ import TopMovie from './assets/components/TopMovies/TopMovie'
 import UpcomingMovie from './assets/components/UpcomingMovie/UpcomingMovie'
 import Navbar from './assets/components/Navbar/Navbar'
 import Footer from './assets/components/Footer/Footer'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function App() {
+  const [movies, setMovies] = useState([])
+
+  const apiCall = async () => {
+
+    try {
+      let data = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_KEY}&language=en-US&page=1`);
+      let finaldata = await data.data
+
+      setMovies(finaldata.results)
+    }
+    catch (err) {
+      console.log(err);
+
+    }
+  }
+
+  useEffect(() => {
+    apiCall();
+  }, [])
+
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route path='/' element={<Home movies={movies} />} />
         <Route path='/topmovies' element={<TopMovie />} />
         <Route path='/upcoming' element={<UpcomingMovie />} />
-        <Route path='/single' element={<SingleMovie />} />
-
-
+        <Route path='/:id' element={<SingleMovie movies={movies} />} />
       </Routes>
       <Footer />
     </BrowserRouter>
